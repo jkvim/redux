ca6fce23195f6924d5342d43cdba9b9164f6bca6
jkvim <jkvim@outlook.com>
2016-09-23 12:32:45 +0800

    translate compose.js comment

--- src/compose.js
+++ src/compose.js
@@ -3,10 +3,19 @@
  * function can take multiple arguments as it provides the signature for
  * the resulting composite function.
  *
+ * 从右到左组合只有一个参数的函数（除了最右一个函数），最右的函数可以获得多个参数
+ * 因为它提供了签名给组合之后的函数
+ *
  * @param {...Function} funcs The functions to compose.
+ *
+ * funcs 参数是用于组合的函数
+ *
  * @returns {Function} A function obtained by composing the argument functions
  * from right to left. For example, compose(f, g, h) is identical to doing
  * (...args) => f(g(h(...args))).
+ *
+ * 返回值是将参数函数从右到左组合起来的函数，例如，compose(f, g, h) 等价于返回一个
+ * (...args) => f(g(h(...args)))
  */
 
 export default function compose(...funcs) {
